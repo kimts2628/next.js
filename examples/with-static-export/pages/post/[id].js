5f18244c95fa165cb6a0365b107554e54977a7d5
kimts2628 <tsts_@likelion.net>
2021-04-29 11:03:16 +0900

    set

--- examples/with-static-export/pages/post/[id].js
+++ examples/with-static-export/pages/post/[id].js
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import {useRouter} from 'next/router'
 
 export async function getStaticPaths() {
   const response = await fetch(
@@ -19,26 +22,38 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  // fetch single post detail
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${params.id}`
-  )
-  const post = await response.json()
+
+  const post = {no: 'no'}
   return {
     props: post,
   }
 }
 
-export default function Post({ title, body }) {
+export default function Post(props) {
+
+  const [data, setData] = useState({});
+
+  const router = useRouter();
+
+  useEffect(() => {
+    let id = router.asPath.slice(6);
+    axios(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then(res => res.data)
+    .then(data => {
+      setData(data);
+      console.log(data);
+    })
+  }, []);
+
   return (
     <main>
       <Head>
-        <title>{title}</title>
+        <title>{data.title}</title>
       </Head>
 
-      <h1>{title}</h1>
+      <h1>{data.title}</h1>
 
-      <p>{body}</p>
+      <p>{data.body}</p>
 
       <Link href="/">
         <a>Go back to home</a>
